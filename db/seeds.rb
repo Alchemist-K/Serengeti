b97cb31c97855f9f1b917b42464676683665de23
미니똥미니 <pete_shin@minittongminiui-MacBook-Pro.local>
2016-06-20 17:22:35 +0900

    로그인 링크 추가 & 유저 5명 시드

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: "user1@snu.ac.kr", password:"12341234", password_confirmation:"12341234")
+
+User.create(email: "user2@snu.ac.kr", password:"12341234", password_confirmation:"12341234")
+User.create(email: "user3@snu.ac.kr", password:"12341234", password_confirmation:"12341234")
+User.create(email: "user4@snu.ac.kr", password:"12341234", password_confirmation:"12341234")
+User.create(email: "user5@snu.ac.kr", password:"12341234", password_confirmation:"12341234")
