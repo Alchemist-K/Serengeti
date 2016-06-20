e929a5e69e3021da32e60571bef9b38d9ee115b4
aprilsblue <jg0428.lim@gmail.com>
2016-06-20 08:24:06 +0000

    team list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Team.create(name: 'team1', abstract: '멋진 팀입니다.', content: '정말정말 멋진1 팀입니다.', number: 5, picture: '1.png')
+Team.create(name: 'team2', abstract: '예쁜 팀입니다.', content: '정말정말 멋진2 팀입니다.', number: 4, picture: '2.png')
+Team.create(name: 'team3', abstract: '화려한 팀입니다.', content: '정말정말 멋진3 팀입니다.', number: 7, picture: '3.png')
+Team.create(name: 'team4', abstract: '큰 팀입니다.', content: '정말정말 멋진4 팀입니다.', number: 5, picture: '4.png')
+Team.create(name: 'team5', abstract: '작은 팀입니다.', content: '정말정말 멋진5 팀입니다.', number: 5, picture: '5.png')
+Team.create(name: 'team6', abstract: '뚱뚱한 팀입니다.', content: '정말정말 멋진6 팀입니다.', number: 6, picture: '6.png')
+Team.create(name: 'team7', abstract: '날씬한 팀입니다.', content: '정말정말 멋진7 팀입니다.', number: 5, picture: '7.png')
+Team.create(name: 'team8', abstract: '조용한 팀입니다.', content: '정말정말 멋진8 팀입니다.', number: 5, picture: '8.png')
+Team.create(name: 'team9', abstract: '멋진 팀입니다.', content: '정말정말 멋진9 팀입니다.', number: 5, picture: '9.png')
