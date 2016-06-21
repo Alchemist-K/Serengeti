334e1968ecb92ab1e6166fbf6d1efec08da5de85
hyunyoungyoon <yoonhy557@gmail.com>
2016-06-21 12:29:51 +0000

    edit user information

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621050925) do
+ActiveRecord::Schema.define(version: 20160621115417) do
 
   create_table "notices", force: :cascade do |t|
     t.string   "title"
@@ -72,6 +72,10 @@ ActiveRecord::Schema.define(version: 20160621050925) do
     t.string   "university"
     t.string   "favor"
     t.string   "name"
+    t.string   "major"
+    t.string   "interest1"
+    t.string   "interest2"
+    t.string   "interest3"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
