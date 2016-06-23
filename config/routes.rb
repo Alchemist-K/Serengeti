7b9579f3802172656d76156257e6534947e21b8d
sdu6342 <uk6342@gmail.com>
2016-06-23 06:02:21 +0000

    회원가입후 인증을 진행해달라는 안내메시지!

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,9 @@ Rails.application.routes.draw do
   get 'mypage/member' => 'mypage#member'
   get 'mypage/myinfo' => "mypage#myinfo"
   get 'admin/index'
+
+
+  get 'home/confirm_please'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
