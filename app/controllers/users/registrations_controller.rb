7ba8f2c2ec3f95f0dff118eea8586a37749e9cbb
미니똥미니 <ecovic94@gmail.com>
2016-06-24 23:55:22 +0900

    phone_number attriute added

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -52,7 +52,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     add_attrs = [:name, :university, :favor, :email, :password, :password_confirmation, :remeber_me, :major,
-    :interest1, :interest2, :interest3]
+    :interest1, :interest2, :interest3, :phone_number]
     devise_parameter_sanitizer.permit(:account_update, keys: add_attrs)
   end
 
