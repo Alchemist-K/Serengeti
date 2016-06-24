5b791de4302cb19de1f640ee237906619272e948
alchemist_k <kimkyungdo08@gmail.com>
2016-06-24 15:22:37 +0000

    메일건 설정 - 가입승인매일 보내기

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,15 +41,15 @@ Rails.application.configure do
   
   # 메일건 관련 초기화
   config.action_mailer.default_url_options = {
-    host: 'serengetiemail-alchemist-k.c9users.io' #host page adress
+    host: 'serengeti.team' #host page adress
   }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
     authentication: :plain,
     address: "smtp.mailgun.org",
     port: 2525,
-    domain: "sandbox166ec72845984d65bfd149eca8920452.mailgun.org",
-    user_name: "postmaster@sandbox166ec72845984d65bfd149eca8920452.mailgun.org",
-    password: "80f0dab811c8ca3195a5bf82fc86818a"
+    domain: "serengeti.team.mailgun.org",
+    user_name: "postmaster@serengeti.team.mailgun.org",
+    password: "9626bb6d9b61ea6dfbbaf334ecf33b25"
   }
 end
