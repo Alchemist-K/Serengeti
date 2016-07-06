df804b306441943e4a3a016647ea4191232c3ea3
KimKyungdo <KyungdoKim@Ks-MacBook-Air.local>
2016-07-06 14:40:56 +0900

    server error test

--- app/controllers/teambuilding_list_controller.rb
+++ app/controllers/teambuilding_list_controller.rb
@@ -1,13 +1,14 @@
 class TeambuildingListController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @teams = Team.all.reverse
     # perform a paginated query:
     @page = Team.all.paginate(:page => params[:page], per_page: 12)
-    
+
+    @timeevent = Team.all.order(:created_at)
     # or, use an explicit "per page" limit:
-    
+
     @button_tags = Serengeti.get_all_tags
   end
 
@@ -16,9 +17,9 @@ class TeambuildingListController < ApplicationController
     sunspot_arr = Sunspot.search(Team) do
       fulltext params[:search_key]
     end
-    
-    @tag_team = sunspot_arr.results 
-  
+
+    @tag_team = sunspot_arr.results
+
     respond_to do |format|
       format.js {render :action => 'tag_search.js.erb'}
     end
@@ -32,14 +33,14 @@ class TeambuildingListController < ApplicationController
     else
       is_custom = false;
     end
-    
+
     @tag_team = Team.all
-    
+
     if is_custom
       @tag_team = @tag_team.select{|x| x.customTags.include?(tag)}
     else
       @tag_team = @tag_team.select{|x| x.tags.include?(tag)}
     end
   end
-  
+
 end
