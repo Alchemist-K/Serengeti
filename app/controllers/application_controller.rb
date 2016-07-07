c6499e032fbf5cf0e0625b995ba491d7f4d00989
KimKyungdo <KyungdoKim@Ks-MacBook-Air.local>
2016-07-07 13:46:05 +0900

    권한 setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
   def ispermitted
     user = current_user
-    if user.name == nil || user.phone_number == nil || user.university == nil || user.major == nil || user.name == "" || user.phone_number == "" || user.university == "" || user.major == ""
+    if user.name == nil || user.phone_number == nil || user.university == nil || user.major == nil || user.name == "" || user.phone_number == "" || user.university == "" || user.major == "" || user.admin == false
       redirect_to "/mypage/myinfo"
     else
       return true
