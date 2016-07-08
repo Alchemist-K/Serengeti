1d1f07a8cb5b42332a83ca514ab3be32bf333865
KimKyungdo <KyungdoKim@Ks-MacBook-Air.local>
2016-07-08 23:11:52 +0900

    server seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(email: "admin@snu.ac.kr", password:"12341234", password_confirmation:"12341234", admin: true, name: "admin", university: "SNU", major: "likelion", phone_number: "010-123-1234", confirmed_at: "2016-06-23 05:07:09")
-User.create(email: "user1@snu.ac.kr", password:"12341234", password_confirmation:"12341234", admin: false, name: 'aaa', confirmed_at: "2016-06-23 05:07:09")
-User.create(email: "user2@snu.ac.kr", password:"12341234", password_confirmation:"12341234", admin: false, name: 'bbb', confirmed_at: "2016-06-23 05:07:09")
-User.create(email: "user3@snu.ac.kr", password:"12341234", password_confirmation:"12341234", admin: false, name: 'ccc', confirmed_at: "2016-06-23 05:07:09")
-User.create(email: "user4@snu.ac.kr", password:"12341234", password_confirmation:"12341234", admin: false, name: 'ddd', confirmed_at: "2016-06-23 05:07:09")
-User.create(email: "user5@snu.ac.kr", password:"12341234", password_confirmation:"12341234", admin: false, name: 'eee', confirmed_at: "2016-06-23 05:07:09")
+User.create(email: "admin@snu.ac.kr", password:"serengeti2016snu", password_confirmation:"serengeti2016snu", admin: true, name: "admin", university: "대학", major: "likelion", phone_number: "010-123-1234", confirmed_at: "2016-06-23 05:07:09")
